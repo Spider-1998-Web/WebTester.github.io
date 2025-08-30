1e0783d9c46e116ce3a0cd472428d6bd9f518fcb
Aman <zbat37@gmail.com>
2025-08-30 11:51:00 +0530

    Sopt light navbar fix 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
-const menuBtn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-const menuIcon = document.getElementById("menu-icon");
-const closeIcon = document.getElementById("close-icon");
 
-let isOpen = false;
 
 AOS.init({
   duration: 800,
   once: true,
 });
 
-menuBtn.addEventListener("click", () => {
-  isOpen = !isOpen;
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+    const menuIcon = document.getElementById('menu-icon');
+    const closeIcon = document.getElementById('close-icon');
 
-  if (isOpen) {
-    menu.style.maxHeight = menu.scrollHeight + "px"; // expand smoothly
-    menuIcon.classList.add("hidden");
-    closeIcon.classList.remove("hidden");
-  } else {
-    menu.style.maxHeight = "0"; // collapse smoothly
-    menuIcon.classList.remove("hidden");
-    closeIcon.classList.add("hidden");
-  }
-});
+    let isOpen = false;
+
+    menuBtn.addEventListener('click', () => {
+      isOpen = !isOpen;
+
+      if (isOpen) {
+        menu.style.maxHeight = menu.scrollHeight + "px"; // expand smoothly
+        menuIcon.classList.add('hidden');
+        closeIcon.classList.remove('hidden');
+      } else {
+        menu.style.maxHeight = "0"; // collapse smoothly
+        menuIcon.classList.remove('hidden');
+        closeIcon.classList.add('hidden');
+      }
+    });
+  });
 
 function openLightbox(src, type) {
   const lightbox = document.getElementById("lightbox");
